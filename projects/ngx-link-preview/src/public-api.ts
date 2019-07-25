02b38ac9d9cb4e9556fd27291069938f6900e001
btxtiger <btxtiger@outlook.com>
2019-07-25 02:11:10 +0200

    Fix public api refs

--- projects/ngx-link-preview/src/public-api.ts
+++ projects/ngx-link-preview/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ngx-link-preview
  */
 export * from './lib/ngx-link-preview.module';
-export * from './lib/services/ngx-link-preview-cache.service.js';
+export * from './lib/services/ngx-link-preview-cache.service';
+export * from './lib/services/ngx-link-preview-loading.manager';
 export * from './lib/components/ngx-link-preview/ngx-link-preview.component';
-export * from './lib/interfaces/open-graph-meta-data.js';
+export * from './lib/interfaces/open-graph-meta-data';
