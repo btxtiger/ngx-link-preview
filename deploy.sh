cbd8616c713d649567a0325841c5b45abd3569b2
btxtiger <btxtiger@outlook.com>
2020-04-14 23:12:41 +0200

    Fix deployment with Angular@9

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ng build ngx-link-preview
+ng build ngx-link-preview --prod
 cp ./README.md dist/ngx-link-preview/
 cp ./LICENSE dist/ngx-link-preview/
 cd dist/ngx-link-preview
